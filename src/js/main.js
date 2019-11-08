e95da82c43ed6e0ccae63f2d9ab655728c710306
Jocelyn Casteletti <jocelyncasteletti@gmail.com>
2019-11-08 00:02:17 -0300

    se agrega bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,46 @@ const stocksInput = document.getElementById('stocks_input');
 const stocksProcess = document.getElementById('stocks_process');
 const stocksResult = document.getElementById('stocks_result');
 
-stocksProcess.addEventListener('click', () => {
+stocksProcess.addEventListener('click', (event) => {
+  event.preventDefault();
+
   const inputValue = stocksInput.value.split(',');
 
-  stocksResult.innerText = bestProfit(inputValue);
+  stocksResult.value = bestProfit(inputValue);
 });
 
 const urlShorteningInput = document.getElementById('url_shortening_input');
 const urlShorteningProcess = document.getElementById('url_shortening_process');
 const urlShorteningResult = document.getElementById('url_shortening_result');
 
-urlShorteningProcess.addEventListener('click', () => {
+urlShorteningProcess.addEventListener('click', (event) => {
+  event.preventDefault();
+
   const inputValue = urlShorteningInput.value;
 
-  urlShorteningResult.innerText = generateShortUrl(inputValue);
+  urlShorteningResult.value = generateShortUrl(inputValue);
 });
 
 const urlUnshorteningInput = document.getElementById('url_unshortening_input');
 const urlUnshorteningProcess = document.getElementById('url_unshortening_process');
 const urlUnshorteningResult = document.getElementById('url_unshortening_result');
 
-urlUnshorteningProcess.addEventListener('click', () => {
+urlUnshorteningProcess.addEventListener('click', (event) => {
+  event.preventDefault();
+
   const inputValue = urlUnshorteningInput.value;
 
   let result = unshortenUrl(inputValue);
 
   if (result === false) {
-    result = 'La URL corta es inválida.';
+    urlUnshorteningResult.classList.add('is-invalid');
+    urlUnshorteningResult.nextElementSibling.classList.add('invalid-feedback');
+    urlUnshorteningResult.nextElementSibling.innerText = 'La URL corta es inválida.';
+    urlUnshorteningResult.value = '';
+  } else {
+    urlUnshorteningResult.classList.remove('is-invalid');
+    urlUnshorteningResult.nextElementSibling.classList.remove('invalid-feedback');
+    urlUnshorteningResult.nextElementSibling.innerText = '';
+    urlUnshorteningResult.value = result;
   }
-
-  urlUnshorteningResult.innerText = result;
 });
